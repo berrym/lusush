e0942f0fd4be9c44210771a58acae5c04a9c1dc4
Luny Lususnaturae <lusus@tardis.site>
2009-08-11 02:26:24 -0400

    Fixed some errors in get_input.

--- src/ldefs.h
+++ src/ldefs.h
@@ -6,7 +6,7 @@
 #endif
 #define MAXLINE 4096        // maximum string length
 
-#define PRINT_DEBUG         // print debugging statements
+//#define PRINT_DEBUG         // print debugging statements
 
 //#define USING_READLINE      // use GNU READLINE
 
