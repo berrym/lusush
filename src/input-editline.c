a5bffaca7dcca472a5dc84b428c585e2096a30c9
Michael Berry <trismegustis@gmail.com>
2021-05-07 13:06:58 -0400

    Use fgets when shell instance is NORMAL_SHELL

--- src/input-editline.c
+++ src/input-editline.c
@@ -46,6 +46,16 @@
 static char *line_read = NULL;  // storage for readline and fgets
 
 /**
+ * null_terminate_line:
+ *      Terminate a string with null character instead of a newline.
+ */
+static inline void null_terminate_line(char *s)
+{
+    if (s[strnlen(s, MAXLINE) - 1] == '\n')
+        s[strnlen(s, MAXLINE) - 1] = '\0';
+}
+
+/**
  * free_line_read:
  *      Free memory allocated to line_read.
  */
@@ -98,6 +108,25 @@ char *get_input(FILE *in)
         build_prompt();
         if ((line_read = rl_gets(getenv("PROMPT"))) == NULL)
             return NULL;
+    } else {
+        // Allocate memory for a line of input
+        if ((line_read = calloc(MAXLINE + 1, sizeof(char))) == NULL)
+            error_syscall("get_input: calloc");
+
+        // If the shell is interactive print a prompt string
+        if (shell_type() != NORMAL_SHELL) {
+            build_prompt();
+            printf("%s", getenv("PROMPT"));
+        }
+
+        // Read a line of input
+        if (fgets(line_read, MAXLINE, in) == NULL)
+            return NULL;
+
+        null_terminate_line(line_read);
+
+        if (in == stdin && line_read && *line_read)
+            add_history(line_read);
     }
 
     return line_read;
