d94ce0e1234080276f1fa1768b6eb998c8bc95dc
Michael Berry <trismegustis@gmail.com>
2024-03-19 17:25:14 -0400

    updated test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 echo "Hello, \
 world! \
-It's a \
-fantastic day!"
+It's \
+a \
+fantastic \
+`date +'%A'`!"
 echo 4^6 / 2 = $((4**6/2))
 echo The expanded glob of the current directory looks like this...
 echo *
