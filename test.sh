52109653c5ba372c0b41b1403527dda1c4dc1adc
Michael Berry <trismegustis@gmail.com>
2024-04-18 18:44:01 -0400

    updated test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,15 @@
-echo "Hello, \
-world! \
+echo "\nHello, \
+world!\n\
+\n\
+\tThis\tis\tthe\t\`lusush'\tshell!\n\
+\n\
 It's \
 a \
 fantastic \
-`date +'%A'`!"
+`date +'%A'`!\n"
+echo "\tIn\t$(date +'%B')\t$(date +'%Y')\n"
 echo 4^6 / 2 = $((4**6/2))
-echo The expanded glob of the current directory looks like this...
+echo "\nThe expanded glob \`*' of \`$(echo `pwd`)' looks like this..."
 echo *
-echo Goodbye!
+echo "\nThe detailed directory listing of ~ looks like...\n$(ls -alF --color=force ~)\n"
+echo "Goodbye!\n"
