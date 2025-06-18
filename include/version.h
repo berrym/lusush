0b35dc17bffebe6d6e1e09d16a5766b28e946539
Michael Berry <trismegustis@gmail.com>
2025-06-17 22:57:50 -0400

    version: Update to v0.1.9
    Increment version to v0.1.9 following major parser improvements and critical
    bug fixes that restore full POSIX control structure functionality.

--- include/version.h
+++ include/version.h
@@ -4,8 +4,8 @@
 #define LUSUSH_NAME "lusush"
 #define LUSUSH_VERSION_MAJOR 0
 #define LUSUSH_VERSION_MINOR 1
-#define LUSUSH_VERSION_PATCH 8
-#define LUSUSH_VERSION_STRING "0.1.8"
+#define LUSUSH_VERSION_PATCH 9
+#define LUSUSH_VERSION_STRING "0.1.9"
 #define LUSUSH_DESCRIPTION "POSIX-compliant shell with modern features"
 
 #endif /* VERSION_H */
